bc50d9d1cd501659d370cd60984e980db41ec1a5
ihor-vip <vip20140902@gmail.com>
2023-06-11 15:45:25 +0300

    create header and footer for layout

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,12 +4,10 @@ import Image from "next/image";
 
 import {CustomButtonProps} from "@/types";
 
-
-
-const CustomButton = ({ containerStyles, title, handleClick }: CustomButtonProps) => (
+const CustomButton = ({ containerStyles, title, handleClick, btnType }: CustomButtonProps) => (
     <button
         disabled={false}
-        type={"button"}
+        type={btnType || "button"}
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
