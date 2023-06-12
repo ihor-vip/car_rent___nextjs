6dca95014bbbcfdda5ab62147c0d4a6eb368bfec
ihor-vip <vip20140902@gmail.com>
2023-06-12 12:33:23 +0300

    create Car Card, fetch data from rapidAPI

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,14 +4,24 @@ import Image from "next/image";
 
 import {CustomButtonProps} from "@/types";
 
-const CustomButton = ({ containerStyles, title, handleClick, btnType }: CustomButtonProps) => (
+const CustomButton = ({ containerStyles, title, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => (
     <button
         disabled={false}
         type={btnType || "button"}
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
-        <span className={`flex-1`}>{title}</span>
+        <span className={`flex-1 ${textStyles}`}>{title}</span>
+        {rightIcon && (
+            <div className="relative w-6 h-6">
+                <Image
+                    src={rightIcon}
+                    alt="right icon"
+                    fill
+                    className="object-contain"
+                />
+            </div>
+        )}
     </button>
 );
 
